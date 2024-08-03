e025c3fe7fa601c0b47fa48f86c566e2aaf0862e
Surik4t <surik4t@yandex.ru>
2024-08-03 19:51:15 +0700

    Books list guide complete

--- flask-vue/client/src/router/index.js
+++ flask-vue/client/src/router/index.js
@@ -1,15 +1,34 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import HelloWorld from '@/components/HelloWorld';
+import Ping from '@/components/Ping';
+import Kanban from '@/components/Kanban';
+import Books from '@/components/Books';
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: '/hello',
       name: 'HelloWorld',
       component: HelloWorld,
     },
+    {
+      path: '/ping',
+      name: 'Ping',
+      component: Ping,
+    },
+    {
+      path: '/kanban',
+      name: 'Kanban',
+      component: Kanban,
+    },
+    {
+      path: '/books',
+      name: 'Books',
+      component: Books,
+    },
   ],
+  mode: 'history',
 });
